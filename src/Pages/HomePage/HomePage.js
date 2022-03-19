d2f8fcc402ed85bb9efcd58c3c941f158c8285b0
ghaithdiab <ghaith.diab@hotmail.com>
2022-03-19 10:40:00 +0100

    desgin home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './HomePage.css'
 const HomePage = () => {
   return (
     <div className='background'>
-      <div className='form'>
-          
+      <div className='header'>
+        <h1>HRnet</h1>
+        <Link to='/Employees'>View Current Employees</Link>
+      </div>
+      <div className='form-div'>
+    
       </div>
     </div>  
   )
