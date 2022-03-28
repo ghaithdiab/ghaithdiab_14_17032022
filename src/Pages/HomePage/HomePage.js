f1177d612e0595f3d06864c66b1cb43ade8a5130
ghaithdiab <ghaith.diab@hotmail.com>
2022-03-28 17:38:48 +0200

    form design

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,17 +1,89 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './HomePage.css'
+import Header from '../../components/Header/Header'
+import { useState } from 'react'
+import Select from 'react-select'
+import { departments, states } from '../../data/data'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 const HomePage = () => {
+  const [startDate, setStartDate] = useState(new Date())
+  const [birthday, setBirthDay] = useState(String)
+  const [state, setState] = useState(states[0].value)
   return (
-    <div className='background'>
-      <div className='header'>
-        <h1>HRnet</h1>
-        <Link to='/Employees'>View Current Employees</Link>
+    <div className="background">
+      <Header />
+      <div className="container">
+        <form>
+          <div className="form-container">
+            <div className="left-side">
+              <div className="input-container">
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" id="firstName" placeholder="First Name" />
+              </div>
+              <div className="input-container">
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" id="lastName" placeholder="Last Name" />
+              </div>
+              <div className="input-container">
+                <label htmlFor="birthday">Date of Birth:</label>
+                <DatePicker
+                  id="birthday"
+                  selected={birthday}
+                  onChange={(date) => setBirthDay(date)}
+                  dateFormat="dd/mm/yyyy"
+                  isClearable
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="startDay">Start Day:</label>
+                <DatePicker
+                  id="startDay"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  dateFormat="dd/mm/yyyy"
+                  isClearable
+                />
+              </div>
+            </div>
+            <div className="right-side">
+              <fieldset className="address">
+                <legend>Address</legend>
+                <div className="input-container-right">
+                  <label htmlFor="street">Street:</label>
+                  <input id="street" type="text" placeholder="Street" />
+                </div>
+                <div className="input-container-right">
+                  <label htmlFor="city">City:</label>
+                  <input id="city" type="text" placeholder="City" />
+                </div>
+                <div className="input-container-right">
+                  <label htmlFor="state">State:</label>
+                  <Select
+                    options={states}
+                    aria-label="state"
+                    placeholder={states[0].label}
+                    onChange={(e) => setState(e.value)}
+                    className="select-state"
+                  ></Select>
+                </div>
+                <div className="input-container-right">
+                  <label htmlFor="zip-code">Zip Code:</label>
+                  <input id="zip-code" type="number" placeholder="Zip Code" />
+                </div>
+              </fieldset>
+              <div className="input-container">
+                <label htmlFor="department">Department:</label>
+                <Select options={departments}></Select>
+              </div>
+            </div>
+          </div>
+          <div className="button">
+            <button className="submit">Save</button>
+          </div>
+        </form>
       </div>
-      <div className='form-div'>
-    
-      </div>
-    </div>  
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import './Header.css'
+const Header = () => {
+  const location=useLocation();
+  console.log(location)
+  return (
+    <div className="header">
+      <h1>HRnet</h1>
+      <Link to="/Employees">View Current Employees</Link>
+      {location.pathname==="/Employees"?<Link to="/">Add New Employee</Link>:""}
+    </div>
+  )
+}
+
+export default Header
